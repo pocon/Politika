541a651d177dcaf051cec775b39869f4363fdf70
Patrick O'Connell <paddymelon@padmel.com>
2012-09-03 17:05:15 +1000

    Removed APH stuff and made closer aligned with OA Data

--- members/models.py
+++ members/models.py
@@ -1,25 +1,10 @@
 from django.db import models
 
-class Website(models.Model):
-    text = models.CharField(max_length=256)
-    url = models.URLField()
-    
-    def __unicode__(self):
-        return self.text
-
-class Position(models.Model):
-    position = models.CharField(max_length=256)
-    primary = models.BooleanField() # whether this is the most important position they hold
-
-    def __unicode__(self):
-        return self.position
-
 class Electorate(models.Model):
     electorate = models.CharField(max_length=256)
-    state = models.CharField(max_length=128)
 
     def __unicode__(self):
-        return self.electorate + ', ' + self.state
+        return self.electorate
 
 class Party(models.Model):
     name = models.CharField(max_length=256)
@@ -28,20 +13,19 @@ class Party(models.Model):
         return self.name
 
 class Member(models.Model):
-    mpid = models.CharField(max_length=20)
-    profile_full_url = models.URLField() # Shouldn't really be used unless a problem with below
-    url = models.URLField() # for the pretty/short version that APH provides which redirects to page
-    websites = models.ManyToManyField(Website, null=True, blank=True)
-    positions = models.ManyToManyField(Position, null=True, blank=True)
+    oa_id = models.CharField(max_length=256) #ID in openaustralia: should be of form uk.org.publicwhip/member/x
+    name = models.CharField(max_length=128)
+
+    website = models.URLField(null=True, blank=True)
+    wikipedia = models.URLField(null=True, blank=True)
+    twitter = models.CharField(max_length=128, null=True, blank=True) # Just contains their handle
+
+    position = models.CharField(max_length=256)
     party = models.ForeignKey(Party, null=True, blank=True)
     electorate = models.ForeignKey(Electorate)
     
-    name = models.CharField(max_length=128) # Just First + Last
-    full_name = models.CharField(max_length=256) # Incl. 'The Hon' and MP, MR, etc.
-    vote_name = models.CharField(max_length=256)
-    
-    
-    image = models.URLField()
+    simage = models.ImageField() # Small Image
+    bimage = models.ImageField() # Big Image
 
     def __unicode__(self):
         return self.full_name
