0d3820200d7453b11e7d128f45631a064bb64873
Patrick O'Connell <paddymelon@padmel.com>
2012-09-04 00:44:15 +1000

    Added Edit History

--- bills/models.py
+++ bills/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from django.auth.models import User
+import datetime
+
+class Edit(models.Model):
+    object_id = models.IntegerField()
+    object_type = models.CharField(max_length=128) # Issue, Bill or Ammendment
+
+    editor = models.ForeignKey(User)
+    time = models.DateTimeField()
+
+    title = models.CharField(max_length=128)
+    summary = models.TextField(max_length=2048, blank=True, null=True)
+    detailed = models.TextField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.time = datetime.datetime.now()
+        super(User, self).save(*args, **kwargs)
 
 class Issue(models.Model):
     title = models.CharField(max_length=128)
