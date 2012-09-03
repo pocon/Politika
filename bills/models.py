2c361c793ea0184dedd928c7c470d0f316268106
Patrick O'Connell <paddymelon@padmel.com>
2012-09-04 01:28:39 +1000

    Added Basic Moderation System

--- bills/models.py
+++ bills/models.py
@@ -7,7 +7,8 @@ class Edit(models.Model):
     object_type = models.CharField(max_length=128) # Issue, Bill or Ammendment
 
     editor = models.ForeignKey(User)
-    time = models.DateTimeField()
+    time = models.DateTimeField(blank=True)
+    moderated = models.BooleanField(default=False)
 
     title = models.CharField(max_length=128)
     summary = models.TextField(max_length=2048, blank=True, null=True)
@@ -18,11 +19,61 @@ class Edit(models.Model):
             self.time = datetime.datetime.now()
         super(User, self).save(*args, **kwargs)
 
+
+class Flag(models.Model):
+    edit = models.ForeignKey(Edit)
+    
+    flagger = models.ForeignKey(User)
+    time = models.DateTimeField(blank=True)
+    
+    subject = models.CharField(max_length=128)
+    reason = models.TextField()
+
+    sustained = models.BooleanField(default=False) # If choose to uphold flag (eg: edit should be reverted)
+    overturned = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.time = datetime.datetime.now()
+        super(User, self).save(*args, **kwargs)
+
+    def sustain(self, admin):
+        self.sustained = True
+        self.save()
+        self.edit.moderated = True
+        self.edit.save()
+
+    def overturned(self, admin):
+        self.overturned = True
+        self.save()
+
 class Issue(models.Model):
     title = models.CharField(max_length=128)
     summary = models.TextField(max_length=2048, blank=True, null=True)
 
     detailed = models.TextField(blank=True, null=True)
+
+    def flag(self, flagger, subject, reason):
+        e = Edit.objects.filter(object_id=self.id).filter(object_type="Issue").filter(moderated=False).order_by('-time')
+        Flag(edit=e, flagger=flagger, subject=subject, reason=reason).save()
+
+    def edit_history(self):
+        return Edit.objects.filter(object_id=self.id).filter(object_type="Issue").order_by('-time')
+
+    def edit(self, editor, title, summary, detailed):
+        Edit(
+            object_id = self.id,
+            object_type = "Issue",
+            editor = editor,
+            title = title,
+            summary = summary,
+            detailed = detailed,
+            ).save()
+
+        self.title = title
+        self.summary = summary
+        self.detailed = detailed
+        self.save()
 
 
 class Bill(models.Model):
@@ -34,6 +85,28 @@ class Bill(models.Model):
     introduced = models.DateField()
     introduced_by = models.ForeignKey(Member, blank=True, null=True)
 
+    def flag(self, flagger, subject, reason):
+        e = Edit.objects.filter(object_id=self.id).filter(object_type="Bill").filter(moderated=False).order_by('-time')
+        Flag(edit=e, flagger=flagger, subject=subject, reason=reason).save()
+
+    def edit_history(self):
+        return Edit.objects.filter(object_id=self.id).filter(object_type="Bill").order_by('-time')
+
+    def edit(self, editor, title, summary, detailed):
+        Edit(
+            object_id = self.id,
+            object_type = "Bill",
+            editor = editor,
+            title = title,
+            summary = summary,
+            detailed = detailed,
+            ).save()
+
+        self.title = title
+        self.summary = summary
+        self.detailed = detailed
+        self.save()
+
 class Ammendment(models.Model):
     title = models.CharField(max_length=256)
     summary = models.TextField(max_length=2048, blank=True, null=True)
@@ -42,3 +115,31 @@ class Ammendment(models.Model):
 
     introduced = models.DateField()
     introduced_by = models.ForeignKey(Member, blank=True, null=True)
+
+    def flag(self, flagger, subject, reason):
+        e = Edit.objects.filter(object_id=self.id).filter(object_type="Ammendment").filter(moderated=False).order_by('-time')
+        Flag(edit=e, flagger=flagger, subject=subject, reason=reason).save()
+
+    def edit_history(self):
+        return Edit.objects.filter(object_id=self.id).filter(object_type="Ammendment").order_by('-time')
+
+    def edit(self, editor, title, summary, detailed):
+        Edit(
+            object_id = self.id,
+            object_type = "Ammendment",
+            editor = editor,
+            title = title,
+            summary = summary,
+            detailed = detailed,
+            ).save()
+
+        self.title = title
+        self.summary = summary
+        self.detailed = detailed
+        self.save()
+
+class Malicious_Edit(models.Model):
+    flag = models.ForeignKey(Flag)
+    admin = models.ForeignKey(User)
+
+
--- electorates.py
+++
@@ -1,4 +0,0 @@
-import BeautifulSoup,urllib2,re
-memberid={}
-for member in BeautifulSoup.BeautifulSoup(urllib2.urlopen(urllib2.Request("http://data.openaustralia.org/members/divisions.xml")).read()).findAll("division"):
- memberid[int(re.compile(r"^uk\.org\.publicwhip/cons/([0-9]+)$").search(member["id"]).group(1))]=str(BeautifulSoup.BeautifulSoup(str(member)).findAll("name")[0]["text"])
