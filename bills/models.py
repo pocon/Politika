e9bba35a7d6267b320ee35253811175cbe1af725
Patrick O'Connell <paddymelon@padmel.com>
2012-09-04 00:24:14 +1000

    Added Issue Model

--- bills/models.py
+++ bills/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Issue(models.Model):
+    title = models.CharField(max_length=128)
+    summary = models.TextField(max_length=2048, blank=True, null=True)
+
+    detailed = models.TextField(blank=True, null=True)
+
+
 class Bill(models.Model):
     title = models.CharField(max_length=256)
     summary = models.TextField(max_length=2048, blank=True, null=True)
