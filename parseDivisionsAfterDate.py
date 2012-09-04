62af615dae7337d81e18c248d07efc5921a994bc
Girbit <willw53@fastmail.fm>
2012-09-04 23:22:39 +1000

    Tweaks to division puller to enable writing directly to db rather than return stuff. Pair functionality added. Temporary state file functionality added (lastdatepulled.txt)

--- parseDivisionsAfterDate.py
+++ parseDivisionsAfterDate.py
@@ -7,19 +7,11 @@ temp=urllib2.urlopen(urllib2.Request("http://data.openaustralia.org/scrapedxml/r
 index=BeautifulSoup.BeautifulSoup(temp)
 for linkTag in index.findAll("a"):
 	links.append(str(linkTag["href"]))
-links=links[5:]
+links=links[5:] # Chop off links to things other than the relevant xmls
 
-''' getDivisions(date) retrieves all the votes on motions from the Hansard transcript of the given sitting date. Is only used internally, external interface is to use getDivisionsAfter(date).
-Notes:
+'''
+getDivisions():
 Accepts: a date object, which must exactly be analogous to a date represented by a string in links (getDivisions will convert to yyyy-mm-dd)
-Returns a list "day", day=[master,master,master...]
-Each master is a list representing a single division with:
- master[0]=list containing the id strings of the ayes votes, defined by the regex ^uk\.org\.publicwhip/member/[0-9]+$
- master[1]=list containing the id strings of the noes votes, ""
- master[2]=string containing the motion/question that triggered the division
- master[3]=date/time object representing when master[2] occurred
- master[4]=url link to aph hansard
- Nothing is done with pairs
 '''
 
 def getDivisions(date):
@@ -29,33 +21,32 @@ def getDivisions(date):
 	day=date.day
 	if day<10:day=str("0"+str(day))
 	dateAsString=str(year)+"-"+str(month)+"-"+str(day)+".xml" # Ensures months and days are at least 2 digits long
-	divisionsForDate=[]
+	print dateAsString
 	if dateAsString in links:
 		rawHansard=urllib2.urlopen(urllib2.Request("http://data.openaustralia.org/scrapedxml/representatives_debates/"+dateAsString)).read()
 		procHansard=BeautifulSoup.BeautifulSoup(rawHansard)
 		for division in procHansard.findAll("division"): # Parse each <division>
-			master=[]
-			master.append([])
-			master.append([])
-			master.append("")
-			master.append(date)
-			master.append("")
 			members=division.findAll("member")
 			for member in members:
-				if member.parent.name=="memberlist": # Ignore <member>s within <pair></pair>
-					if member["vote"]=="aye":master[0].append(str(member["id"]))
-					else:master[1].append(str(member["id"]))# Assuming all votes are either "aye" or "no" and there are no errors in the xml
+				if member.parent.name=="memberlist":
+					if member["vote"]=="aye": pass#member["id"] will give the id for the aye vote
+					else:pass# As above. Also: assuming all votes are either "aye" or "no" and there are no errors in the xml
+				elif member.parent.name=="pairs": # process the pairs
+					pairmembers=member.parent.findAll("member")
+					# pairmembers[0]["id"] and pairmembers[1]["id"] give you the id for each member
+				else:pass # do nothing with member tags not in a division
 			if division.findPreviousSibling().name=="speech": # assuming the reason for division is given in the <p> childs of the first <speech> tag before the <division> tag
+				reason=""
 				for pTag in division.findPreviousSibling().findAll("p"):
-					if pTag.string!=None:master[2]+=str(pTag.string)
-			master[4]=str(division["url"])
-			divisionsForDate.append(master)
+					if pTag.string!=None:reason+=str(pTag.string)
+					# variable reason now contains a string representing the apparent reason for the division
+			# You can get the url of the division in the official hansard from aph with: str(division["url"])
+			# date gives the date on which the division occurred as a date object
 	else:
 		raise ValueError,"No transcripts for that date"
-	return divisionsForDate
 
 
-'''getDivisionsAfter(date) accepts a date object and returns a list of division lists, which are defined according to master, representing all divisions on or after that date
+'''getDivisionsAfter(date) accepts a date object and pulls all divisions after that date (NOT including that date)
 '''
 def getDivisionsAfter(date):
 	final=[]
@@ -67,11 +58,22 @@ def getDivisionsAfter(date):
 		templinks.append(tempDate) # convert links to a list of date objects
 	for cdate in templinks: # Iterate through the dates until we reach the date we want, and parse all dates after that
 		if reachedDate:
-			final+=getDivisions(cdate)
+			getDivisions(cdate)
 		else:
 			if cdate.year>=date.year:
 				if cdate.month>=date.month:
 					if cdate.day>=date.day:
 						reachedDate=True
-						final+=getDivisions(cdate)
-	return final
+	writeNewDate=open("lastdatepulled.txt","w")
+	writeNewDate.write(str(cdate.year)+"\n"+str(cdate.month)+"\n"+str(cdate.day))
+	writeNewDate.close()
+
+try:
+	rawpdate=open("lastdatepulled.txt","rU") #lastdatepulled.txt is the "counter", the last date that was pulled, a 3 line text file the first line the year, the second the month and the third the day
+	datelimit=datetime.date(int(rawpdate.readline().rstrip()),int(rawpdate.readline().rstrip()),int(rawpdate.readlin(),rstrip()))
+	rawpdate.close()
+	getDivisionsAfter(datelimit)
+except IOError:
+	getDivisionsAfter(datetime.date(2010,8,21)) #If lastdatepulled.txt does not exist or cannot be read, program will auto-pull all divisions after 21 August 2010, the last election
+except Exception,e:
+	print e
